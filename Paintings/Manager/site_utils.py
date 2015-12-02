f3ec81441d9773782dc6950f4403c726c9fdd3f3
Gregory Vigo Torres <gregory.vigo@gmail.com>
2015-12-02 18:22:53 +0100

    console script:delete unused images
    Still doesn't search for unused js and css

--- Paintings/Manager/site_utils.py
+++ Paintings/Manager/site_utils.py
@@ -1,10 +1,35 @@
 import re
 from pathlib import Path
+import os
+from itertools import chain
 
 from flask.ext.script import (Command, prompt, prompt_pass)
 from flask import current_app
 
 from Paintings.core import db
+from Paintings.models.public import Image
+
+class UnusedFiles(Command):
+    def __init__(self):
+        self.app = current_app
+
+    def run(self):
+        """ Query db for images and scan static dirs for files
+            Remove unused files (mostly images added during development/testing)
+            and potentially unused css or js files
+        """
+        img_root = self.app.config['STATIC_IMAGE_ROOT']
+        # STATIC_THUMBNAIL_ROOT ## is beneath img_root
+        image_q = db.session.query(Image.filename).all()
+        images_fn = [i[0] for i in sorted(image_q, key=lambda i:i[0])]
+
+        img_paths = chain.from_iterable([[Path(r, fn) for fn in f] for r, d, f in os.walk(img_root)])
+        del_paths = [i for i in img_paths if i.name not in images_fn]
+        
+        for i in sorted(del_paths, key=lambda i:i.name) :
+            print('deleting {}'.format(i))
+            i.unlink()
+
 
 class MakeRobotsTxt(Command):
     def __init__(self):
--- manage.py
+++ manage.py
@@ -4,12 +4,14 @@ from Paintings.App import create_app
 from Paintings.Manager.Users import (CreateUser, DeleteUser)
 from Paintings.Manager.InitApp import InitApp
 from Paintings.Manager.DbUtils import UpdateOrder
+from Paintings.Manager.site_utils import UnusedFiles
 
 manager = Manager(create_app)
 manager.add_option('-c', '--config', dest='config', required=False)
 manager.add_command('create_user', CreateUser)
 manager.add_command('delete_user', DeleteUser)
 manager.add_command('update_image_order', UpdateOrder)
+manager.add_command('remove_unused_files', UnusedFiles)
 manager.add_command('init', InitApp)
 
 if __name__ == '__main__':
