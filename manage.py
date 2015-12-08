9daf8e9f8c6e0b153a6793de53db42cce73bfb46
Gregory Vigo Torres <gregory.vigo@gmail.com>
2015-12-08 14:08:11 +0100

    Removed InitApp from Manager
    because all the functionality is included in app.before_first_request

--- manage.py
+++ manage.py
@@ -2,7 +2,6 @@
 from flask.ext.script import Manager
 from App.App import create_app
 from App.Manager.Users import (CreateUser, DeleteUser)
-from App.Manager.InitApp import InitApp
 from App.Manager.DbUtils import UpdateOrder
 from App.Manager.site_utils import UnusedFiles
 
@@ -12,7 +11,6 @@ manager.add_command('create_user', CreateUser)
 manager.add_command('delete_user', DeleteUser)
 manager.add_command('update_image_order', UpdateOrder)
 manager.add_command('remove_unused_files', UnusedFiles)
-manager.add_command('init', InitApp)
 
 if __name__ == '__main__':
     manager.run()
